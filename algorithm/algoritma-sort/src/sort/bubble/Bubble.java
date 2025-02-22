feb245cd61f5257ef45289d0d120a30b1599f713
jayameiman <workingsdd@gmail.com>
2025-02-22 13:33:00 +0700

    [JAVATUTORIAL][sort-algorithm]-implementation bubble sort

---
+++ algorithm/algoritma-sort/src/sort/bubble/Bubble.java
@@ -0,0 +1,26 @@
+package sort.bubble;
+
+import java.util.Arrays;
+
+public class Bubble {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println("before sort : "+ Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println("after sort : "+ Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int temp = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
--- algoritma-sort/src/sort/bubble/Bubble.java
+++
@@ -1,4 +0,0 @@
-package sort.bubble;
-
-public class Bubble {
-}
